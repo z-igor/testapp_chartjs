0cdc4c809396dc48e78dd6594542b891829c8924
z-igor <z-igor@nomail.com>
2020-10-18 13:55:38 +0300

    deploy Test-App-with-vue-chartjs-material-design

---
+++ precache-manifest.84cd5cb1b0c3e697ed194526847882eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df611d82cd9001937ed4",
+    "url": "/testapp_chartjs/css/app.db7a4400.css"
+  },
+  {
+    "revision": "7236297e8968a18504ed",
+    "url": "/testapp_chartjs/css/chunk-vendors.5699e6ca.css"
+  },
+  {
+    "revision": "30e4d5b703614a339c978b2b11a42f16",
+    "url": "/testapp_chartjs/index.html"
+  },
+  {
+    "revision": "df611d82cd9001937ed4",
+    "url": "/testapp_chartjs/js/app.3fe72fce.js"
+  },
+  {
+    "revision": "423c64c76c02aa27098b",
+    "url": "/testapp_chartjs/js/chunk-70e6e212.ccb135a5.js"
+  },
+  {
+    "revision": "7236297e8968a18504ed",
+    "url": "/testapp_chartjs/js/chunk-vendors.5107899c.js"
+  },
+  {
+    "revision": "d673a4cf853577cb0b3bc439edd96829",
+    "url": "/testapp_chartjs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testapp_chartjs/robots.txt"
+  }
+]);
